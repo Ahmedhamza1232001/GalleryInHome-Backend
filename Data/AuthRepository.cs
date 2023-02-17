c18d2582ff35797b192e1e029d3cece24a29545d
AhmedHamza <ahmedhamza7895123@gmail.com>
2023-02-17 15:24:34 +0200

    implement register

--- Data/AuthRepository.cs
+++ Data/AuthRepository.cs
@@ -7,14 +7,38 @@ namespace Proj.Data
 {
     public class AuthRepository : IAuthRepository
     {
+        public DataContext Context { get; }
+        public AuthRepository(DataContext context)
+        {
+            this.Context = context;
+
+        }
         public Task<ServiceResponse<string>> Login(string username, string password)
         {
             throw new NotImplementedException();
         }
 
-        public Task<ServiceResponse<int>> Register(User user, string password)
+        public async Task<ServiceResponse<int>> Register(User user, string password)
         {
-            throw new NotImplementedException();
+            CreatePasswordHash(password, out byte[] passwordHash, out byte[] passwordSalt);
+
+            user.PasswordHash = passwordHash;
+            user.PasswordSalt = passwordSalt;
+
+            this.Context.Users.Add(user);
+            await this.Context.SaveChangesAsync();
+            ServiceResponse<int> response = new ServiceResponse<int>();
+            response.Data = user.Id;
+            return response;
+        }
+
+        private void CreatePasswordHash(string password, out byte[] passowrdHash, out byte[] passwordSalt)
+        {
+            using (var hmac = new System.Security.Cryptography.HMACSHA512())
+            {
+                passwordSalt = hmac.Key;
+                passowrdHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+            }
         }
 
         public Task<bool> UserExist(string username)
