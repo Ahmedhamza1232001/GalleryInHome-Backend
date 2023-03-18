de44731414d2ba57918c1e338d72a79f3776a391
AhmedHamza <ahmedhamza7895123@gmail.com>
2023-03-18 14:05:21 +0200

    some changes

--- Program.cs
+++ Program.cs
@@ -63,10 +63,12 @@ builder.Services.AddScoped<IFactoryService, FactoryService>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+app.UseSwagger();
+app.UseSwaggerUI();
+
+
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
