861020c588df8599f9fa969ba1498ee97cc0c976
AhmedHamza <ahmedhamza7895123@gmail.com>
2023-03-24 05:20:45 +0200

    git all products for user

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -13,9 +13,21 @@ namespace Proj.Controllers
 {
     [ApiController]
     [Route("api/[controller]")]
-    [Authorize]
+    
     public class UserController:ControllerBase
     {
+        private readonly IProductService ProductService;  
+        public UserController(IProductService productService)
+        {
+            this.ProductService = productService;
+        }
+
+        [HttpGet("GetAll")]
+        public async Task<ActionResult<ServiceResponse<List<StakeholderGetProductDto>>>> Get()
+        {
+            return Ok(await this.ProductService.GetAllProducts());
+        }
+        
         
     }
 }
