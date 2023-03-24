af68a3def76510d81a04fb496f2dbf23e6dfc1b4
AhmedHamza <ahmedhamza7895123@gmail.com>
2023-03-24 20:57:11 +0200

    remove warnings from models

--- models/Factory.cs
+++ models/Factory.cs
@@ -10,7 +10,7 @@ namespace Proj.models
     {
         public int Id { get; set; }
         public string Name { get; set; } = string.Empty;
-        public Product Product { get; set; }
+        public Product? Product { get; set; }
         public int ProductId { get; set; }
 
     }
--- models/Material.cs
+++ models/Material.cs
@@ -9,6 +9,6 @@ namespace Proj.models
     {
         public int Id { get; set; }
         public string Name { get; set; } = string.Empty;
-        public List<Product> Products { get; set; }
+        public List<Product> Products { get; set; } = new List<Product>();
     }
 }
