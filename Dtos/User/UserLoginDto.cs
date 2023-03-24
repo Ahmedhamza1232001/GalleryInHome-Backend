4069d437050fa958e96259375137cbbeb7c45290
AhmedHamza <ahmedhamza7895123@gmail.com>
2023-03-24 21:18:25 +0200

    user login dto modfication

--- Dtos/User/UserLoginDto.cs
+++ Dtos/User/UserLoginDto.cs
@@ -9,10 +9,7 @@ namespace Proj.Dtos.User
     {
         public string UserName { get; set; } = string.Empty;
         public string Password { get; set; } = string.Empty;
-        public string FirstName { get; set; } = string.Empty;
-        public string LastName { get; set; } = string.Empty;
-        public int Age { get; set; }
-        public string Email { get; set; } = string.Empty;
-        public string Token { get; set; } = string.Empty;
+      
+        
     }
 }
