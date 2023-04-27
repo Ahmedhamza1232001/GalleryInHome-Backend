4d368630abf893cfd63ab6587553975bd492c974
AhmedHamza <ahmedhamza7895123@gmail.com>
2023-04-27 18:41:27 +0200

    remove null warnings

--- Dtos/Product/User/GetProductDto.cs
+++ Dtos/Product/User/GetProductDto.cs
@@ -15,15 +15,15 @@ namespace Proj.Dtos.Product
         public double Price { get; set; }
         public int Discount { get; set; }
         public ColorClass Color { get; set; } = ColorClass.White;
-        public List<Image> Images { get; set; } 
+        public List<Image>? Images { get; set; }
         public int Height { get; set; }
         public int Width { get; set; }
         public int Depth { get; set; }
         public string MadeIn { get; set; } = string.Empty;
         public int Warranty { get; set; }
         public bool IsFavo { get; set; }
-        public GetFactoryDto Factory { get; set; }
-        public List<GetMaterialDto> Materials { get; set; }
+        public GetFactoryDto? Factory { get; set; }
+        public List<GetMaterialDto>? Materials { get; set; }
         public bool InStock { get; set; } = true;
         public string Model { get; set; } = string.Empty;
         public string Review { get; set; } = string.Empty;
--- models/Product.cs
+++ models/Product.cs
@@ -15,16 +15,16 @@ namespace Proj.models
         public double Price { get; set; }
         public int  Discount { get; set; }
         public ColorClass Color { get; set; } = ColorClass.White;
-        public List<Image> Images { get; set; } = new List<Image>();
-        public int Height { get; set; }   
+        public List<Image>? Images { get; set; }
+        public int Height { get; set; }
         public int Width { get; set; }
         public int Depth { get; set; }
         public string MadeIn { get; set; } = string.Empty;
         public int Warranty { get; set; }
-        public List<Material> Materials { get; set; } = new List<Material>();
+        public List<Material>? Materials { get; set; }
         public bool IsFavo { get; set; }
         public User? User { get; set; }
-        public Factory? Factory { get; set; } 
+        public Factory? Factory { get; set; }
         public bool InStock { get; set; } = true;
         public string Model { get; set; } = string.Empty;
         public string Review { get; set; } = string.Empty;
